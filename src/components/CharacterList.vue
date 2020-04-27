6fb4836a80ad0dec4aa683c86c3de109cc3c43a3
Jack Quigley <jackaquigley@gmail.com>
2020-04-27 21:50:47 +0100

    film details now brings up characters height and name

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div if="characters">
+<ul>
+  <li v-for="(character, index) in characters" :key="index" :characters="characters">
+    <p>{{character.name}}</p>
+    <p>{{character.height}}cm</p>
+  </li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'character-list',
+  props: ['characters', 'character']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/components/FilmDetail.vue
+++ src/components/FilmDetail.vue
@@ -4,14 +4,44 @@
   <p>{{film.episode_id}}</p>
   <p>{{film.release_date}}</p>
   <p>{{film.director}}</p>
+
+  <character-list v-if="characters.length" :characters="characters"></character-list>
 </div>
 </template>
 
 <script>
+
+import CharacterList from './CharacterList.vue'
+
+
 export default {
   name: 'film-detail',
-  props: ['film']
-}
+  props: ['film'],
+  components: {
+    'character-list': CharacterList
+  },
+  data() {
+    return {
+      characters: []
+    }
+  },
+  methods: {
+    getCharacters() {
+      const characterPromises = this.film.characters.map((characterData) => {
+      return fetch(characterData).then(res => res.json())
+  })
+  Promise.all(characterPromises)
+  .then(data => this.characters = data);
+  }
+},
+mounted(){
+  this.getCharacters();
+},
+watch:{
+  film: function(){this.getCharacters();
+      }
+    }
+  }
 </script>
 
 <style lang="css" scoped>
