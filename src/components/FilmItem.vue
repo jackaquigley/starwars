0be600c4e765ea78e3fd18626b7a7cf9daafcdd8
Jack Quigley <jackaquigley@gmail.com>
2020-04-27 21:13:11 +0100

    set up seperate page for FilmItem

---
+++ src/components/FilmItem.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <li>{{film.title}}</li>
+</template>
+
+<script>
+export default {
+  name: 'film-item',
+  props: ['film']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
